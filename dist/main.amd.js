794b25f7389654edd8964e40a0bc6fbb725d7064
Ryan Florence <rpflorence@gmail.com>
2013-11-12 15:17:37 -0700

    allow styling based on open state

--- dist/main.amd.js
+++ dist/main.amd.js
@@ -222,6 +222,7 @@ ic.MenuTriggerComponent = Ember.Component.extend({
   },
 
   openList: function(event) {
+    event.preventDefault();
     // I have no idea how reliable this is, but it seems good enough
     this.set('lastClickEventWasMouse', event.clientX > 0 && event.clientY > 0);
     if (this.closingClickStarted) {
@@ -249,9 +250,15 @@ ic.MenuComponent = Ember.Component.extend({
 
   classNames: 'ic-menu',
 
+  classNameBindings: ['isOpen:is-open:is-closed'],
+
   list: null,
 
   listTrigger: null,
+
+  isOpen: function() {
+    return this.get('list.isOpen');
+  }.property('list.isOpen'),
 
   registerList: function(list) {
     this.set('list', list);
@@ -288,7 +295,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   
 
 
-  data.buffer.push("<style>\n.ic-menu-list {\n  position: absolute;\n  display: none;\n}\n\n.ic-menu-list[aria-expanded=\"true\"] {\n  display: block;\n}\n\n.ic-menu-list {\n  outline: none;\n  background: #fff;\n  border: 1px solid #aaa;\n  border-radius: 3px;\n  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.25);\n  margin: 4px 0;\n  list-style-type: none;\n  padding: 4px 0px;\n  font-family: \"Lucida Grande\", \"Arial\", sans-serif;\n  font-size: 12px;\n}\n\n.ic-menu-item {\n  padding: 4px 20px;\n  cursor: default;\n  white-space: nowrap;\n}\n\n.ic-menu-item:focus {\n  background: #3879D9;\n  color: #fff;\n  outline: none;\n}\n</style>\n\n");
+  data.buffer.push("<style>\n.ic-menu-list {\n  position: absolute;\n  display: none;\n}\n\n.ic-menu-list[aria-expanded=\"true\"] {\n  display: block;\n}\n\n.ic-menu-list {\n  outline: none;\n  background: #fff;\n  border: 1px solid #aaa;\n  border-radius: 3px;\n  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.25);\n  list-style-type: none;\n  padding: 2px 0px;\n  font-family: \"Lucida Grande\", \"Arial\", sans-serif;\n  font-size: 12px;\n}\n\n.ic-menu-item {\n  padding: 4px 20px;\n  cursor: default;\n  white-space: nowrap;\n}\n\n.ic-menu-item:focus {\n  background: #3879D9;\n  color: #fff;\n  outline: none;\n}\n</style>\n\n");
   
 });
 
