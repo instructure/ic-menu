fe30b5a6ec8ea168222932181bdf4f01a794bf6a
Ryan Florence <rpflorence@gmail.com>
2013-12-12 13:22:45 -0700

    actual amd/cjs support

--- dist/main.js
+++ dist/main.js
@@ -341,6 +341,16 @@
 });
 
 
++function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['ember'], function(Ember) { return factory(Ember); });
+  } else if (typeof exports === 'object') {
+    factory(require('ember'));
+  } else {
+    factory(Ember);
+  }
+}(this, function(Ember) {
+
 Ember.TEMPLATES["components/ic-menu-css"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
@@ -377,4 +387,6 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   data.buffer.push("\n");
   return buffer;
   
+});
+
 });
